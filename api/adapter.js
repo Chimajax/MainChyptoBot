e71da3e320671cfca5849a09c78d809116e93f99
Chimajax <chinedubaka2022@gmail.com>
2025-03-26 18:57:08 +0100

    Delete api/adapter.js

--- api/adapter.js
+++
@@ -1,9 +0,0 @@
-// api/adapter.js
-const { bot } = require('../bot1');
-
-module.exports = async (update) => {
-  // Convert Telegraf-style update to node-telegram-bot-api format
-  if (update.message) {
-    await bot.processUpdate(update);
-  }
-};
